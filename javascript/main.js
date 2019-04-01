7663d2409c8b4659222c9af30184f0b775d182da
Myrthe van Eijk <44472129+myrtheveijk@users.noreply.github.com>
2019-04-01 21:00:53 +0200

    For loop ports
    Ports en for loop toegevoegd, overige code weggehaald

--- javascript/main.js
+++ javascript/main.js
@@ -1,42 +1,25 @@
-// let lights = ["orange", "green", "yellow", "blue"];
-// let sounds = ["./sounds/bmo.wav", "./sounds/linux.wav", "./sounds/check.wav", "./sounds/play.wav"];
-//
-// let light = document.getElementById('light');
-// let sound = document.getElementById('sound');
-//
-// let orange = document.getElementById('orange');
-// orange.addEventListener("click", function (){
-//     light.src = lights[0];
-//     sound.src = sounds[0];
-//     sound.play();
-//     console.log("bmo!");
-// });
+let ports = ["A1", "A2", "A5", "A6"];
 
+let buttons = {
+    "A1":{colorName: "orange",   colorCode: "0xff8000", sound: "Sounds.BaDing"},
+    "A2":{colorName: "green",    colorCode: "0x00ff00", sound: "Sounds.BaDing"},
+    "A5":{colorName: "yellow",   colorCode: "0xffff00", sound: "Sounds.BaDing"},
+    "A6":{colorName: "blue",     colorCode: "0x00ffff", sound: "Sounds.BaDing"}
+};
 
-//OBJECT: kleuren, kleurcodes en geluiden
-let buttons = [
-    {colorName: "orange",   colorCode: "0xff8000", sound: "Sounds.BaDing"},
-    {colorName: "green",    colorCode: "0x00ff00", sound: "Sounds.BaDing"},
-    {colorName: "yellow",   colorCode: "0xffff00", sound: "Sounds.BaDing"},
-    {colorName: "blue",     colorCode: "0x00ffff", sound: "Sounds.BaDing"}
-];
-
-
-//FUNCTION: Wanneer je op de sensor drukt > licht en geluid.
 function pressure(){
     let pressure = 0;
     forever(function () {
-        pressure = pins.A2.analogRead();
-        if (pressure >= 50) {
-            music.playSoundUntilDone(music.sounds(Sounds.BaDing));
-            light.setAll(buttons.colorCode)
-        } else {
-            light.setAll(0x000000)
+        for (let port of ports) {
+            pressure = pins[port].analogRead();
+            if (pressure >= 50) {
+                music.playSoundUntilDone(music.sounds(Sounds.BaDing));
+                light.setAll(buttons[port].colorCode)
+            } else {
+                light.setAll(0x000000)
+            }
         }
     });
 }
 
 pressure();
-
-
-
