d8f0eddaec3d50d0ae51d2364407d9d44abcc057
Julia <julialanu@hotmail.com>
2019-04-03 13:04:59 +0200

    Code gecleaned

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,3 @@
-// let ports = ["A1", "A2", "A5", "A6"];
-
-// let buttons = {
-//     "A1":{colorName: "orange",   colorCode: "0xff8000", sound: "Sounds.BaDing"},
-//     "A2":{colorName: "green",    colorCode: "0x00ff00", sound: "Sounds.BaDing"},
-//     "A5":{colorName: "yellow",   colorCode: "0xffff00", sound: "Sounds.BaDing"},
-//     "A6":{colorName: "blue",     colorCode: "0x00ffff", sound: "Sounds.BaDing"}
-// };
-
-// function pressure(){
-//     let pressure = 0;
-//     forever(function () {
-//         for (let port of ports) {
-//             pressure = pins[port].analogRead();
-//             if (pressure >= 50) {
-//                 music.playSoundUntilDone(music.sounds(Sounds.BaDing));
-//                 light.setAll(buttons[port].colorCode)
-//             } else {
-//                 light.setAll(0x000000)
-//             }
-//         }
-//     });
-// }
-
-// pressure();
-
 let Press4 = 0
 let Press3 = 0
 let Press2 = 0
