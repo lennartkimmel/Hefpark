269ef5320d1450b281f7371d6270e4d0d91f4f1d
myrthe.e <myrthe.e@hotmail.nl>
2019-04-08 13:55:21 +0200

    Sounds & Pressure
    Andere tonen en lagere druk

--- javascript/main.js
+++ javascript/main.js
@@ -8,38 +8,40 @@ forever(function () {
     Press3 = pins.A5.analogRead()
     Press4 = pins.A6.analogRead()
     for (let index = 0; index <= 9; index++) {
-        if (index * 50 <= Press1) {
+        if (index * 10 <= Press1) {
             light.onboardStrip().setPixelColor(index, 0xff8000)
         } else {
             light.onboardStrip().setPixelColor(index, 0x000000)
         }
-        if (Press1 >= 50) {
-            music.playSoundUntilDone(music.sounds(Sounds.MagicWand))
+        if (Press1 >= 10) {
+            music.playTone(208, music.beat(BeatFraction.Eighth))
         }
-        if (index * 50 <= Press2) {
-            light.onboardStrip().setPixelColor(index, 0xffff00)
-        } else {
-            light.onboardStrip().setPixelColor(index, 0x000000)
-        }
-        if (Press2 >= 50) {
-            music.playSoundUntilDone(music.sounds(Sounds.PowerUp))
-        }
-        if (index * 50 <= Press3) {
-            light.onboardStrip().setPixelColor(index, 0x00ff00)
-        } else {
-            light.onboardStrip().setPixelColor(index, 0x000000)
-        }
-        if (Press3 >= 50) {
-            music.playSoundUntilDone(music.sounds(Sounds.Siren))
-        }
-        if (index * 50 <= Press4) {
+        if (index * 10 <= Press2) {
             light.onboardStrip().setPixelColor(index, 0x0000ff)
         } else {
             light.onboardStrip().setPixelColor(index, 0x000000)
         }
-        if (Press4 >= 50) {
-            music.playSoundUntilDone(music.sounds(Sounds.JumpDown))
+        if (Press2 >= 10) {
+            music.playTone(277, music.beat(BeatFraction.Eighth))
         }
+        if (index * 10 <= Press3) {
+            light.onboardStrip().setPixelColor(index, 0xffff00)
+        } else {
+            light.onboardStrip().setPixelColor(index, 0x000000)
+        }
+        if (Press3 >= 10) {
+            music.playTone(330, music.beat(BeatFraction.Eighth))
+        }
+        if (index * 10 <= Press4) {
+            light.onboardStrip().setPixelColor(index, 0x00ff00)
+        } else {
+            light.onboardStrip().setPixelColor(index, 0x000000)
+        }
+        if (Press4 >= 10) {
+            music.playTone(370, music.beat(BeatFraction.Eighth))
+        }
+        light.setAll(0x000000)
     }
+    music.setVolume(256)
     pause(500)
 })
